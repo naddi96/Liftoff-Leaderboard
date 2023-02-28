622de74c45c63073b1582dce04936293a5786e14
naddi96 <nardidamiano@gmail.com>
2023-02-28 14:05:54 +0100

    added automatic renewal

--- proxy_server/nginx.Dockerfile
+++ proxy_server/nginx.Dockerfile
@@ -1,14 +1,9 @@
-
 FROM nginx:latest
-
 RUN apt update
-RUN apt install certbot -y && apt install python3-certbot-nginx -y
-COPY "./default.conf" "/etc/nginx/conf.d/"
-copy "./start-nginx.sh" "/"
-#/etc/nginx/conf.d/default.conf
-
-#RUN certbot --nginx -d liftoff.mywire.org -m d.naddi.96@gmail.com  --agree-tos -n
+RUN apt install cron -y
+RUN apt install certbot -y
+RUN apt install python3-certbot-nginx -y
+COPY "./default.conf.template" "/etc/nginx/conf.d/"
+COPY "./start-nginx.sh" "/"
 RUN chmod a+x start-nginx.sh
 CMD ["./start-nginx.sh"]
-
-#certbot certonly --standalone -d liftoff.mywire.org -m d.naddi.96@gmail.com  --agree-tos -n && sleep 3 && certbot --nginx -d liftoff.mywire.org -m d.naddi.96@gmail.com  --agree-tos -n && sleep 3 && nginx -g 'daemon off;'
--- proxy_server/start-nginx.sh
+++ proxy_server/start-nginx.sh
@@ -1,11 +1,14 @@
+export DOLLAR="$"
+envsubst < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+echo 'SHELL=/bin/sh 
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin 
+0 */12 * * * root certbot renew -q --post-hook "service nginx reload"' > /etc/cron.d/certbot
+/etc/init.d/cron start
 
-if [ -f "/etc/letsencrypt/live/l.liftoff.mywire.org/fullchain.pem" ]; then
+
+if [ -f "/etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem" ]; then
     nginx -g 'daemon off;'
 else 
-    certbot certonly --standalone -d l.liftoff.mywire.org -m d.naddi.96@gmail.com  --agree-tos -n
-    certbot --nginx -d l.liftoff.mywire.org -m d.naddi.96@gmail.com  --agree-tos -n
+    certbot certonly --standalone -d $DOMAIN_NAME -m $EMAIL  --agree-tos -n
+    nginx -g 'daemon off;'
 fi
-
-
-
-
